92632ca4321d1375f21707475b21922e674cd6f4
ajzal-byte <ajzalaju10@gmail.com>
2024-05-27 14:17:59 +0530

    Hero Section updated

---
+++ src/components/ui/index.ts
@@ -0,0 +1,8 @@
+import { Spotlight } from "./Spotlight";
+import { TextGenerateEffect } from "./TextGenerateEffect";
+import MagicButton from "./MagicButton";
+import { FloatingNav } from "./FloatingNav";
+
+
+
+export { Spotlight, TextGenerateEffect, MagicButton, FloatingNav };
