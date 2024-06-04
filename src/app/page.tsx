c4353a73312d455053d955e715b6cf31f3d0c692
ajzal-byte <ajzalaju10@gmail.com>
2024-06-04 20:41:53 +0530

    bugs fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import { navItems } from "@/data";
 
 export const Home = () => {
   return (
-    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+    <main className="relative bg-black-100 flex justify-center items-center flex-col mx-auto sm:px-10 px-5 overflow-clip">
       <div className="max-w-7xl w-full">
         <FloatingNav
           navItems={navItems}
